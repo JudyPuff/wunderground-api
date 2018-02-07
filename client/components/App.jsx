0b41d16c1089ae43e5d31009a261aeedb4dcf664
Judy Ting-Edwards <judy.yun.ting@gmail.com>
2018-02-07 17:48:42 +1300

    finished project

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import Weather from './Weather'
+import request from 'superagent'
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      weatherObject: this.getWeather()
+    }
+    this.getWeather = this.getWeather.bind(this)
+  }
+
+  getWeather () {
+    request
+      .get('https://api.wunderground.com/api/2fad0b0fdeeafdf8/conditions/q/NZ/Eden_Terrace.json')
+      .then(res => {
+        this.setState({
+          weatherObject: res.body
+        })
+      })
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>EDA Auckland&rsquo;s Weather</h1>
+        {this.state.weatherObject && <Weather weather={this.state.weatherObject} />}
+      </div>
+    )
+  }
 }
 
 export default App
-
---
+++ client/components/Weather.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Weather = (props) => {
+  return (
+    <div>
+      <div>
+        <h3>
+          Currently: {props.weather.current_observation.weather} &nbsp;
+          <img src={props.weather.current_observation.icon_url} alt="current observation" />
+        </h3>
+        <h2>
+          {props.weather.current_observation.temp_c}&deg;C ({props.weather.current_observation.temp_f}&deg;F)
+        </h2>
+        <h4>
+          Wind from the {props.weather.current_observation.wind_dir} at {props.weather.current_observation.wind_kph}kph
+        </h4>
+        
+      </div>
+      <div>
+        <p>{props.weather.current_observation.observation_time}</p>
+        <img src={props.weather.current_observation.image.url} alt="weatherunderground logo" className="pic"/>
+      </div>
+    </div>
+  )
+}
+
+export default Weather
